9996190f394125400f4aae4c77e159a7aeca5a45
Simplyalex99 <alexmflores99@hotmail.ca>
2021-09-18 15:02:43 -0400

    style: updated home.css

--- front-end/src/pages/home.jsx
+++ front-end/src/pages/home.jsx
@@ -3,11 +3,17 @@ import "../styles/home.css";
 export const Home = () => {
   return (
       <section className="grid text-center justify-content-center" id="home">
+     
           <div></div>
+          <div className="content-container">
       <h1>
         Find reviews instantly with <span className="text-primary">AI</span>
-          </h1>
-          <div></div>
+              </h1>
+              </div>
+          <div>
+              
+              </div>
+           
     </section>
   );
 };

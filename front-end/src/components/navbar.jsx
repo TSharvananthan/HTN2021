07c52887672951a94e341eb95b0d2d530ba0c2f6
Simplyalex99 <alexmflores99@hotmail.ca>
2021-09-18 13:43:50 -0400

    style: navbar

--- front-end/src/components/navbar.jsx
+++ front-end/src/components/navbar.jsx
@@ -5,24 +5,29 @@ export const Navbar = () => {
     <div id="nav" className="nav w-100 h-50">
       <div className="container">
         <div className=" navbar nav ">
-          <p className="text-light">
+          <p className="text-light brand">
             Review<span className="text-primary">AI</span>
-          </p>
+                  </p>
+                  
+                  {/*  navigation links */ }
           <ul className="d-flex flex-row">
-            <li className="nav-item nav-link">
-              <a className="text-light" href="#">
+            <li className="nav-link text-light">
+              <a className="text-light bg-gray-link" href="#">
                 How it works
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
+            <li className="nav-link text-light bg-gray-link">
+              <a className="text-light bg-gray-link" href="#">
                 Inspiration
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link">About us</a>
+            <li className="nav-link">
+              <a className="text-light bg-gray-link">About us</a>
             </li>
-          </ul>
+                  </ul>
+                  
+
+
           <form class="form-inline my-2 my-lg-0">
             <button class="btn btn-primary my-2 my-sm-0" type="submit">
               Feedback

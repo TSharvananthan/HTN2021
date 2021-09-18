a006a9183d2e4121222ef0fcc537bdecec3f5a0b
Simplyalex99 <alexmflores99@hotmail.ca>
2021-09-18 15:06:21 -0400

    style: updated home.css

--- front-end/src/pages/home.jsx
+++ front-end/src/pages/home.jsx
@@ -2,18 +2,23 @@ import React from "react";
 import "../styles/home.css";
 export const Home = () => {
   return (
-      <section className="grid text-center justify-content-center" id="home">
-     
-          <div></div>
-          <div className="content-container">
-      <h1>
-        Find reviews instantly with <span className="text-primary">AI</span>
-              </h1>
-              </div>
-          <div>
-              
-              </div>
-           
+    <section className="grid text-center justify-content-center" id="home">
+      <div></div>
+      <div className="content-container">
+        <h1>
+          Find reviews instantly with <span className="text-primary">AI</span>
+        </h1>
+        <p>
+          Simplifying customer reviews for you so you can find the best
+          restaurant, save money, and get instant feedback...
+        </p>
+        <form class="form-inline my-2 my-lg-0">
+          <button class="btn btn-primary my-2 my-sm-0" type="submit">
+            Start searching
+          </button>
+        </form>
+      </div>
+      <div></div>
     </section>
   );
 };

6ca7628ce6b1b073fef20d3ac16a034e8f14e3b7
Simplyalex99 <alexmflores99@hotmail.ca>
2021-09-18 15:09:17 -0400

    style: updated call to action button

--- front-end/src/pages/home.jsx
+++ front-end/src/pages/home.jsx
@@ -13,7 +13,7 @@ export const Home = () => {
           restaurant, save money, and get instant feedback...
         </p>
         <form class="form-inline my-2 my-lg-0">
-          <button class="btn btn-primary my-2 my-sm-0" type="submit">
+          <button type="button" class="btn btn-primary my-2 my-sm-0 btn-lg btn-block w-100" type="submit">
             Start searching
           </button>
         </form>

0c1266891dafe64bd40cad7d8ace66f0c4fa1a19
Simplyalex99 <alexmflores99@hotmail.ca>
2021-09-18 18:48:39 -0400

    feat: added landing page

--- front-end/src/pages/home.jsx
+++ front-end/src/pages/home.jsx
@@ -4,13 +4,8 @@ import "../styles/home.css";
 export const Home = () => {
     return (
       <>
-   
-          <section
-            className="text-center"
-            id="home"
-            >
-
-                <div class=" container grid justify-content-center">
+        <section className="text-center" id="home">
+          <div class=" container grid justify-content-center">
             <div></div>
             <div className="content-container">
               <div class="proto-container">
@@ -36,14 +31,45 @@ export const Home = () => {
                 </button>
               </form>
             </div>
-                    <div></div>
-                    </div>
-          </section>
-   
+            <div></div>
+          </div>
+        </section>
+
         <section className="text-center section-b">
           <div className="img-bg"></div>
-          <div class="content">
-            <h2>How it works</h2>
+          <div class=" container content">
+            <h2 class="pb-5">How it works</h2>
+            <p>
+              Enter the name of the business and see a list of reviews that are
+              simplified and classified as positive or negative!
+              <a> Try it out now! </a>
+            </p>
+            <p>
+              To receive feedback from a review you can visit FeedBack section
+              to write an anonymnous review and our machine learning model will
+              do some cool tricks and give back a feedback of a sentimental
+              analysis of the review. <a> Try it out now! </a>
+            </p>
+          </div>
+        </section>
+        <section className="text-center section-c">
+          <div class=" container ">
+            <h2 class="pb-5">Inspiration</h2>
+            <p>
+              Every year, thousands of new restaurants open up for business, but
+              how do restaurants know how customers are feeling? It takes time
+              to find the right views and often there is ambiguity. This brought
+              us an idea to help business and as welll as customers to have an
+              app to find lots of views that are simplified with no more
+              wondering how a customer feel, and allows others to find a place
+              for their family and friends to enjoy a nice meal.
+            </p>
+          </div>
+        </section>
+        <section className="text-center section-d">
+          <div className="img-bg"></div>
+          <div class=" container content">
+            <h2 class="pb-5">How it works</h2>
             <p>
               Enter the name of the business and see a list of reviews that are
               simplified and classified as positive or negative!
